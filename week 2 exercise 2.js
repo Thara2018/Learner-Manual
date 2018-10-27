a08f855d3ac868fac5ad06a5966beccb0a7b8c4e
Thara <tharanivks@gmail.com>
2018-10-27 13:04:49 +0800

    Week 2 Tutorial

---
+++ week 2 exercise 2.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>JavascriptExercise1</title>
+	<script type="text/javascript">
+    
+var triangle:"#";
+for(var i=0; i<7;i++) {
+triangle=triangle+"#";
+console.log(triangle);
+}
